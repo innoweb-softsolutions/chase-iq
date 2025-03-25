00125fc2b14e18af3f0396a5f2c0779e90eb5bc6
Mahmoud Trkumani <mahmoud-swe@hotmail.com>
2025-03-26 02:37:14 +0300

    Optimization
    Moved to using Expected conditions for faster code execution
    
    Saved around 1-2 minutes of execution time.
    
    Also added more error catching clauses to provide more meaningful error logs

--- src/ApolloScraper.py
+++ src/ApolloScraper.py
@@ -1,4 +1,7 @@
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
@@ -13,23 +16,25 @@ import os
 
 # Fills in the user details and presses login
 def login_google(browser):
-    loginID = browser.find_element(By.ID, "identifierId")
+    loginID = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.ID, "identifierId"))
+    )
     loginID.send_keys(os.getenv('GOOGLE_EMAIL'))
     loginID.send_keys(Keys.ENTER)
 
-    time.sleep(3)
-
-    passField = browser.find_element(By.CSS_SELECTOR, "input[type='password']")
+    passField = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[type='password']"))
+    )
     passField.send_keys(os.getenv('GOOGLE_PASSWORD'))
     passField.send_keys(Keys.ENTER)
-
-    time.sleep(30)
 
 # For some reason, the location filters here require that you press them
 # This means that unlike the other filters, you need to use ActionChains to simulate user interaction
 def location_filter(browser, locationTab):
     locationTab.click()
-    searchField = browser.find_element(By.CLASS_NAME, 'Select-input')
+    searchField = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, 'Select-input'))
+    )
     actions = ActionChains(browser)
     actions.move_to_element(searchField).click().perform()
 
@@ -39,32 +44,41 @@ def location_filter(browser, locationTab):
         searchField.send_keys(location)
         time.sleep(3)
         actions.move_to_element(searchField).click().perform()
+        first_child = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "Select-option"))
+        )
+        first_child.click()
         time.sleep(1)
-        searchField.send_keys(Keys.ENTER)
-        time.sleep(4)
 
 # Filter out the jobs using the list provided
 def job_filter(browser, jobTab):
     jobTab.click()
     
     # Insert the job titles as filters
-    searchField = browser.find_element(By.CSS_SELECTOR, 'div.Select-placeholder')
-    searchField = browser.find_element(By.CLASS_NAME, 'Select-input')
+    searchField = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, 'Select-input'))
+    )
     jobTitlesStr = os.getenv('JOB_TITLES')
     # Load the job titles first
     for job in jobTitlesStr.split(',') if jobTitlesStr else []:
         searchField.send_keys(job)
-        time.sleep(3)
-        searchField.send_keys(Keys.ENTER)
-        time.sleep(4)
+        first_child = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "Select-option"))
+        )
+        first_child.click()
+        time.sleep(1)
 
     # Only include C-Suite results
-    time.sleep(4)
-    browser.find_element(By.XPATH, "//div[div/span[text()='Departments & Job Function']]").click()
-    time.sleep(4)
-    browser.find_element(By.XPATH, "//div[text()='C-Suite']/preceding-sibling::div").click()
-    time.sleep(4)
-
+    cSuiteBtn = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//div[div/span[text()='Departments & Job Function']]"))
+    )
+    cSuiteBtn.click()
+    time.sleep(3)
+    cSuiteOption = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//div[text()='C-Suite']/preceding-sibling::div"))
+    )
+    cSuiteOption.click()
+    
 def show_all_emails(browser):
     allEmailButtons = browser.find_elements(By.XPATH, "//button[span[text()='Access email']]")
 
@@ -74,26 +88,32 @@ def show_all_emails(browser):
 
 def collect_data(browser, dataList, dataListClean, shouldCollectEmail):
     # Set based on the free limit of the service
-    for pageNumber in range(2):
+    for pageNumber in range(3):
 
         # Assuming there are 25 records in each page
+        if(shouldCollectEmail):
+            print('[INFO] Collecting Emails')
+            show_all_emails(browser)
+        else:
+            print('[INFO] Skipping email collection (enable from settings)')
+        
         for x in range(25):
-            firstRow = browser.find_element(By.ID, 'table-row-' + str(x))
+            firstRow = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.ID, 'table-row-' + str(x)))
+            )
             dataRow = firstRow.find_elements(By.CLASS_NAME, 'zp_KtrQp')
 
-            if(shouldCollectEmail):
-                print('[INFO] Collecting Emails')
-                show_all_emails()
-            else:
-                print('[INFO] Skipping email collection (enable from settings)')
-            
             currRowData = []
             for cell in dataRow:
                 currRowData.append(cell.text)
 
-            # There tends to be an empty element at the start of each row
-            linkedInHref = firstRow.find_element(By.CSS_SELECTOR, "i.zp-icon.apollo-icon.apollo-icon-linkedin").find_element(By.XPATH, "..").get_attribute("href")
+            # Need to ensure that the LinkedIn profile link exists for this row
+            linkedInHref = 'N/A'
+            if(len(firstRow.find_elements(By.CSS_SELECTOR, "i.zp-icon.apollo-icon.apollo-icon-linkedin")) > 0):
+                linkedInHref = firstRow.find_element(By.CSS_SELECTOR, "i.zp-icon.apollo-icon.apollo-icon-linkedin").find_element(By.XPATH, "..").get_attribute("href")
             currRowData[6] = linkedInHref
+            
+            # There tends to be an empty element at the start of each row
             currRowData.pop(0)
             dataList.append(currRowData)
             # Now for the cleaned data
@@ -106,17 +126,18 @@ def collect_data(browser, dataList, dataListClean, shouldCollectEmail):
             dataListClean.append(currRowCleaned)
 
         # Navigate to next page
-        browser.find_element(By.CSS_SELECTOR, 'button.zp_qe0Li.zp_S5tZC > .apollo-icon-chevron-arrow-right').click()
-        time.sleep(7)
-
+        nextPageBtn = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.zp_qe0Li.zp_S5tZC > .apollo-icon-chevron-arrow-right'))
+        )
+        nextPageBtn.click()
 
 def ApolloScraper():
     print('[INFO] Loading configuration')
     # Point towards the .env file that contains the config
     envPath = Path(__file__).resolve().parents[1] / 'config' / '.env'
-    # load_dotenv(dotenv_path=envPath)
+
     if not load_dotenv(dotenv_path=envPath):
-        raise FileNotFoundError(f"Error: .env file not found at {envPath}")
+        raise FileNotFoundError(f"[Error] .env file not found at {envPath}")
     else:
         print(f"[INFO] Successfully loaded .env file from {envPath}")
 
@@ -125,25 +146,26 @@ def ApolloScraper():
     try:
         browser.get('https://app.apollo.io/#/login')
 
-        time.sleep(3)
-
         # Find the Login with Google button
         print('[INFO] Attempting Google Login')
-        elem = browser.find_element(By.CSS_SELECTOR, "button.zp-button.zp_GGHzP.zp_Kbe5T.zp_PLp2D.zp_rduLJ.zp_g5xYz")
+        elem = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "button.zp-button.zp_GGHzP.zp_Kbe5T.zp_PLp2D.zp_rduLJ.zp_g5xYz"))
+        )
         elem.click()
-        time.sleep(3)
-
+        
         # Login using Google
         login_google(browser)
 
         # Navigate to the appropriate tab
-        browser.find_element(By.ID, 'side-nav-people').click()
+        peopleTab = WebDriverWait(browser, 30).until(
+            EC.element_to_be_clickable((By.ID, 'side-nav-people'))
+        )
+        peopleTab.click()
         time.sleep(4)
 
         print('[INFO] Setting location filters')
         # Get the filter tabs
         tabs = browser.find_elements(By.CSS_SELECTOR, 'div.zp-accordion-header.zp_r3aQ1')
-        time.sleep(4)
         
         # Filter locations
         location_filter(browser, tabs[5])
@@ -156,13 +178,11 @@ def ApolloScraper():
         # Only include verified emails
         tabs[2].click()
         browser.find_element(By.CLASS_NAME, 'zp_vcdPP').click()
-        time.sleep(4)
-
+        
         print('[INFO] Scrapping data')
         userDataList = []
         userDataListClean = []
-        time.sleep(6)
-
+        
         # Collect all the user data into a list for CSV/JSON storage
         isCollectEmails = bool(strtobool(os.getenv('COLLECT_EMAILS')))
         collect_data(browser, userDataList, userDataListClean, isCollectEmails)
@@ -191,13 +211,18 @@ def ApolloScraper():
             writer.writerows(userDataListClean)
 
         print('Everything went well!')
-        # time.sleep(600)
-
+        
     except FileNotFoundError as fnfe:
         print(fnfe)
 
+    except NoSuchElementException:
+        print("[Error] Failed to find an element in the browser while scraping. Aborting.")
+
+    except TimeoutError:
+        print('[ERROR] Timed out while trying to find element.')
+
     except Exception as e:
-        print(f"Ran into an error")
+        print('[ERROR] An error occurred.')
 
     finally:
         print("Completed")
